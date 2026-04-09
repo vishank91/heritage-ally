6bc637f321683a4627e1664f47954a44548707b7
unknown <vishankchauhan@gmail.com>
2026-04-09 13:52:16 +0530

    Navbar Updated 09-04-2026

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,5 +3,6 @@ import React from 'react'
 export default function Footer() {
   return (
     <div>Footer</div>
+    
   )
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,3 +5,4 @@ export default function Navbar() {
     <div>Navbar</div>
   )
 }
+
